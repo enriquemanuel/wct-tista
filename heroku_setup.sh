ef925799152962a5d632efd4dbc3142509079c07
Enrique Valenzuela <evalenzuela@evalenzuela.local>
2019-08-29 23:43:46 -0400

    correct syntax

--- heroku_setup.sh
+++ heroku_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=$(cat .version)
-echo 'export SECRET_SAUCE='$((1 + RANDOM % 10))
-echo 'export PATH_TO_DATA_FILE=data'
-echo 'export WCT_VERSION='$version
+
+export SECRET_SAUCE=$((1 + RANDOM % 10))
+export PATH_TO_DATA_FILE='wct_app/data'
+export WCT_VERSION=$(cat .version)
