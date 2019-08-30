0d45615e6e9ce229bad083ac7486310c9b890359
Enrique Valenzuela <evalenzuela@evalenzuela.local>
2019-08-29 23:52:35 -0400

    try to set the env

--- heroku_setup.sh
+++ heroku_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-export SECRET_SAUCE=$((1 + RANDOM % 10))
-export PATH_TO_DATA_FILE='wct_app/data'
-export WCT_VERSION=$(cat .version)
+echo 'export SECRET_SAUCE='$((1 + RANDOM % 10)) >> bash_env
+echo 'export PATH_TO_DATA_FILE=data' >> bash_env
+echo 'export WCT_VERSION='$version >> bash_env
+source bash_env
