c648a51716cfa92b34a667318f38d8f52f934bf5
Enrique Valenzuela <evalenzuela@evalenzuela.local>
2019-08-30 00:02:09 -0400

    remove unused files

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-release: bash heroku_setup.sh
-
 web: gunicorn --workers=4 --log-level debug wct_app.main.app:APP
--- heroku_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'export SECRET_SAUCE='$((1 + RANDOM % 10)) >> bash_env
-echo 'export PATH_TO_DATA_FILE=data' >> bash_env
-echo 'export WCT_VERSION='$version >> bash_env
-source bash_env
